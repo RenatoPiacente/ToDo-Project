a96e424182071937ba01d077aad9b1b43e7b2189
Renato Piacente <97764409+RenatoPiacente@users.noreply.github.com>
2022-06-28 22:27:58 -0400

    updated urls on app and project

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,7 @@
-from django import path
-
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('helloworld/', views.hello_world),
+    path('', views.task_list, name='task-list'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django import HttpResponse
+from django.http import HttpResponse
 
 def hello_world(Request):
     return HttpResponse('Hello, World!')
+
+
+def task_list(Request):
+    return render(Request, 'tasks/list.html')
+
+
